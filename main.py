c242855350e01e95f62406bcb4b4529c893e70f9
miskiaino <vilanordakill@gmail.com>
2018-11-04 15:44:18 +0500

    gergre

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ now = datetime.datetime.now()
 
 
 def main():
-    new_offset = None
+
     today = now.day
     hour = now.hour
 
@@ -68,7 +68,7 @@ def main():
             greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
             today += 1
 
-        new_offset = last_update_id + 1
+
 
 if __name__ == '__main__':
     try:
