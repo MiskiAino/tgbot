69d41bb95787d230e7c330f3196b2ef7941f0f20
miskiaino <vilanordakill@gmail.com>
2018-11-05 01:36:44 +0500

    ggewr

--- tgbot.py
+++ tgbot.py
@@ -7,13 +7,19 @@ TOKEN = '721378927:AAF3OX-i_oXfK0asbUJzDi5JhVVujShLomI'
 
 bot = telebot.TeleBot(TOKEN)
 
+
+@bot.message_handler(commands=['start','help'])
+def command_handler(message: Message):
+    reply_to(message, str('hello, im is helpless bot'))
+
 @bot.message_handler(content_types=['text'])
 def echo_digits(message: Message):
     bot.reply_to(message, str(random.random()))
 
+import time
+while True:
+    time.sleep(1)
 
-# @bot.message_handler(commands=['start','help'])
-# def command_handler(message: Message):
-#
 
-bot.polling(none_stop=True)
+
+bot.polling(none_stop=True, )
