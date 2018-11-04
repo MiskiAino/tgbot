33751a4cdc9c5964ad752f98ef671980913fc82f
miskiaino <vilanordakill@gmail.com>
2018-11-05 03:39:21 +0500

    ggewr

--- tgbot.py
+++ tgbot.py
@@ -16,7 +16,9 @@ def command_handler(message: Message):
 def echo_digits(message: Message):
     bot.reply_to(message, str(random.random()))
 
-# fdf
+@bot.message_handler(content_types=['sticker'])
+def sticker_handler(message: Message):
+    bot.reply_to(message, 'опять стикеры с оняме')
 
 
 bot.polling(none_stop=True)
